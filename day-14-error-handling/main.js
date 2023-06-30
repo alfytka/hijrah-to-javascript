1c75679193ef285591f22683596ce9053f825e2a
alfytka <lyzexx04@gmail.com>
2023-06-30 09:35:48 +0700

    day 14 of 30 days of learning javascript

---
+++ day-14-error-handling/main.js
@@ -0,0 +1,53 @@
+try {
+   let lastName = 'Haerul'
+   let fullName = firstName + ' ' + lastName
+} catch (err) {
+   console.error(err)
+}
+
+try {
+   let lastName = 'Novanto'
+   let fullName = firstName + ' ' + lastName
+} catch (err) {
+   console.error(err)
+} finally {
+   console.log('In any case I will be executed')
+}
+
+{
+   try {
+      let lastName = 'Novanto'
+      let fullName = firstName + ' ' + lastName
+   } catch (err) {
+      console.log('Name of the error', err.name)
+      console.log('Error message', err.message)
+   } finally {
+      console.log('In any case I will be executed')
+   }
+}
+
+const throwErrorExampleFun = () => {
+   let message
+   let x = prompt('Enter a number')
+   try {
+      if (x == '') throw 'empty'
+      if (isNaN(x)) throw 'not a number'
+      x = Number(x)
+      if (x < 5) throw 'too low'
+      if (x > 10) throw 'too high'
+   } catch (err) {
+      console.log(err)
+   }
+}
+// throwErrorExampleFun()
+
+// let firstName = 'Alfitka'
+// let fullName = firstName + ' ' + lastName
+// console.log(fullName)
+
+// let square = 2 x 2
+// console.log(square)
+console.log('Hello, world')
+
+let num = 10
+console.log(num.toLowerCase())
