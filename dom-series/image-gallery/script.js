c54d236827428ed03429bbd885708b9e05569f00
alfytka <lyzexx04@gmail.com>
2023-08-11 07:42:22 +0700

    javascript series

---
+++ dom-series/image-gallery/script.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+const thumbs = document.querySelectorAll('.thumb');
+
+container.addEventListener('click', function(e) {
+
+   if (e.target.className == 'thumb') {
+      jumbo.src = e.target.src;
+      jumbo.classList.add('fade');
+      setTimeout(function() {
+         jumbo.classList.remove('fade');
+      }, 500);
+      thumbs.forEach(function(thumb) {
+         // if (thumb.classList.contains('active')) {
+         //    thumb.classList.remove('active');
+         // }
+         thumb.className = 'thumb';
+      });
+      e.target.classList.add('active');
+   }
+
+})
+
+
+// thumbs.forEach(function(thumb) {
+//    thumb.addEventListener('click', function(e) {
+//       // jumbo.setAttribute('src', e.name);
+//       jumbo.src = e.target.src;
+//    });
+// });
+
