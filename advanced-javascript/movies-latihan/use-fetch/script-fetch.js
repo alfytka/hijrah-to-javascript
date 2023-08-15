654ec21b0335bf94a542cbd5bb9398cf2685f9a4
alfytka <lyzexx04@gmail.com>
2023-08-15 19:57:20 +0700

    new knowledge: async await

--- advanced-javascript/movies-latihan/use-fetch/script-fetch.js
+++ advanced-javascript/movies-latihan/use-fetch/script-fetch.js
@@ -30,36 +30,115 @@
 // });
 
 // menggunakan fetch
+// const searchButton = document.querySelector('.search-button');
+// searchButton.addEventListener('click', function() {
+
+//    const inputKeyword = document.querySelector('.input-keyword');
+//    fetch('http://www.omdbapi.com/?apikey=dca61bcc&s=' + inputKeyword.value)
+//       .then(response => response.json())
+//       .then(response => {
+//          const movies = response.Search;
+//          let cards = '';
+//          movies.forEach(m => cards += showCards(m));
+//          const movieContainer = document.querySelector('.movie-container');
+//          movieContainer.innerHTML = cards;
+
+//          // ketika tombol detail di-klik
+//          const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+//          modalDetailButton.forEach(btn => {
+//             btn.addEventListener('click', function() {
+//                const imdbid = this.dataset.imdbid;
+//                fetch('http://www.omdbapi.com/?apikey=dca61bcc&i=' + imdbid)
+//                   .then(response => response.json())
+//                   .then(m => {
+//                      const movieDetail = showMovieDetail(m);
+//                      const modalBody = document.querySelector('.modal-body');
+//                      modalBody.innerHTML = movieDetail;
+//                   });
+//             });
+//          });
+//       });
+// });
+
+// Fetch (Refactor)
 const searchButton = document.querySelector('.search-button');
-searchButton.addEventListener('click', function() {
-
-   const inputKeyword = document.querySelector('.input-keyword');
-   fetch('http://www.omdbapi.com/?apikey=dca61bcc&s=' + inputKeyword.value)
-      .then(response => response.json())
-      .then(response => {
-         const movies = response.Search;
-         let cards = '';
-         movies.forEach(m => cards += showCards(m));
-         const movieContainer = document.querySelector('.movie-container');
-         movieContainer.innerHTML = cards;
-
-         // ketika tombol detail di-klik
-         const modalDetailButton = document.querySelectorAll('.modal-detail-button');
-         modalDetailButton.forEach(btn => {
-            btn.addEventListener('click', function() {
-               const imdbid = this.dataset.imdbid;
-               fetch('http://www.omdbapi.com/?apikey=dca61bcc&i=' + imdbid)
-                  .then(response => response.json())
-                  .then(m => {
-                     const movieDetail = showMovieDetail(m);
-                     const modalBody = document.querySelector('.modal-body');
-                     modalBody.innerHTML = movieDetail;
-                  });
-            });
-         });
-      });
+searchButton.addEventListener('click', async function() {
+   try {
+      const inputKeyword = document.querySelector('.input-keyword');
+      const movies = await getMovies(inputKeyword.value);
+      updateUI(movies);
+   } catch(err) {
+      // console.log(err);
+      alert(err);
+   }
 });
 
+function getMovies(keyword) {
+   return fetch('http://www.omdbapi.com/?apikey=dca61bcc&s=' + keyword)
+      .then(response => {
+         if (!response.ok) {
+            throw new Error(response.statusText);
+         }
+         return response.json();
+      })
+      .then(response => {
+         if (response.Response === "False") {
+            throw new Error(response.Error);
+         }
+         return response.Search;
+      });
+}
+
+function updateUI(movies) {
+   let cards = '';
+   movies.forEach(m => cards += showCards(m));
+   const movieContainer = document.querySelector('.movie-container');
+   movieContainer.innerHTML = cards;
+}
+
+// tidak berjalan
+// const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+// modalDetailButton.forEach(btn => {
+//    btn.addEventListener('click', function() {
+//       console.log('ok');
+//    });
+// });
+
+// pake cara ini -> cara ini bisa digunakan untuk event pada elemen yang awalnya tidak ada, kemudian ada
+// event binding
+document.addEventListener('click', async function(e) {
+   if (e.target.classList.contains('modal-detail-button')) {
+      try {
+         const imdbid = e.target.dataset.imdbid;
+         const movieDetail = await getMovieDetail(imdbid);
+         updateUIDetail(movieDetail);
+      } catch(err) {
+         alert(err);
+      }
+   }
+});
+
+function getMovieDetail(imdbid) {
+   return fetch('http://www.omdbapi.com/?apikey=dca61bcc&i=' + imdbid)
+      .then(response => {
+         if (!response.ok) {
+            throw new Error(response.StatusText);
+         }
+         return response.json()
+      })
+      .then(response => {
+         if (response.Response === "False") {
+            throw new Error(response.Error);
+         }
+         return response;
+      });
+}
+
+function updateUIDetail(m) {
+   const movieDetail = showMovieDetail(m);
+   const modalBody = document.querySelector('.modal-body');
+   modalBody.innerHTML = movieDetail;
+}
 
 function showCards(m) {
    return `<div class="col-md-4 my-3">
